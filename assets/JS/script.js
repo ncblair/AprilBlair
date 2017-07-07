f169aab8f7c5ebbfdf921c5a02edce84da9adcb9
Nathan Blair <nblair@berkeley.edu>
2017-07-06 17:27:16 -0700

    added smooth scrolling links

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+
+	//Smooth anchor link scrolling, taken from the web.
+
+	// Add smooth scrolling to all links
+  	$("a").on('click', function(event) {
+  		console.log("click function");
+    	// Make sure this.hash has a value before overriding default behavior
+    	if (this.hash !== "") {
+    		console.log("hash");
+      		// Prevent default anchor click behavior
+      		event.preventDefault();
+
+      		// Store hash
+      		var hash = this.hash;
+
+      		// Using jQuery's animate() method to add smooth page scroll
+      		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      		$('html, body').animate({
+        		scrollTop: $(hash).offset().top
+      		}, 800, function(){
+   
+        		// Add hash (#) to URL when done scrolling (default click behavior)
+        		window.location.hash = hash;
+      		});
+    	} // End if
+  	});
+
+
+
+
 	console.log("hello");
 	function moveScroller() {
     	var $image = $(".background-image-container");
@@ -41,4 +71,6 @@ $(document).ready(function(){
 		move();
 	}
 	moveScroller();
+
+
 });
